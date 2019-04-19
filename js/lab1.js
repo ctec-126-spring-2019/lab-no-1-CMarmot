97a7f7bf04c14de2e20d0c07d7e38de7305e1b0e
CMarmot <corleneankrum@gmail.com>
2019-04-19 11:33:50 -0700

    Fixed errors

--- js/lab1.js
+++ js/lab1.js
@@ -4,26 +4,33 @@ function reverseString(str) {
     return str.split("").reverse().join("");
 }
 
-function listSkills(){
-    let skills = ['Web Development','Gardening','Drumming','Volunteering';
+function listSkills() {
+    // next line needed closing bracket
+    let skills = ['Web Development','Gardening','Drumming','Volunteering'];
     let output = "<h2>Bruce's Skills<h2>";
     output += '<ul>';
     for (let i = 0; i < skills.length; i++) {
         output += '<li>' + skills[i] + '</li>';
     }
     output += '</ul>';
+
     document.getElementById('skills').innerHTML = '<p>' + output + '</p>';
 }
 
-function getStarted(){
-    let firstName  'Bruce'
+function getStarted() {
+
+    // added equal and semicolon to next line
+
+    let firstName = 'Bruce';
     let lastName = 'Elgort';
-    let collegeName = 'Clark College'
+        // added semicolon to next line
+    let collegeName = 'Clark College';
     let city = 'Vancouver, Washington';
     let gpa = 3.2;
-    let output = `${firstName} {lastName} is currently attending ${collegeName}, located in {city} and has a grade point average of {gpa}`;
-
-    document.getElementById('place1').innerHTML = '<p>' + output + '</p>";
+    // added $ to last name, city and gpa
+    let output = `${firstName} ${lastName} is currently attending ${collegeName}, located in ${city} and has a grade point average of ${gpa}`;
+// changed double quote to single quote
+    document.getElementById('place1').innerHTML = '<p>' + output + '</p>';
     document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
 }
 
